c114f9863fe6071e858d70583c7df97fa19a1c93
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-04-20 18:20:25 +0530

    next commit

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { create } from '../controller/user.controller.js';
+import { create, getAllUsers } from '../controller/user.controller.js';
 const route = express.Router();
 route.post("/user",create);
+route.get("/users",getAllUsers)
 export default route;
